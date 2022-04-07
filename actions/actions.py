34840a24f762874236999a2986facb4e689293a6
Christopher Patenaude <patenauc@oregonstate.edu>
2022-04-07 10:26:03 -0700

    Add form validation action

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,34 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import EventType
+
+
+class ValidateGenreForm(FormValidationAction):
+
+    def name(self) -> Text:
+        return "validate_genre_form"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict
+    ) -> List[EventType]:
+        return []
+
+    def validate_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict
+    ) -> Dict[Text, Any]:
+        if not slot_value:
+            return {"genre": None}
+        return {"genre": slot_value}
