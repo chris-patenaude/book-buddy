f717d2f0241bf1f5ce57977e7811c5a5076c1315
Christopher Patenaude <patenauc@oregonstate.edu>
2022-04-15 08:46:18 -0700

    Complete MVP

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# Book Buddy: Conversational AI with Rasa
+
+## Installation
+### Required Dependencies 
+- python3 version: 3.7 or 3.8 
+- pip3 version: latest
+- rasa
+  
+### Windows
+**Installing Rasa**
+```powershell
+    C:\path\to\project\root> pip3 install -U pip
+    C:\path\to\project\root> pip3 install rasa
+```
+
+**Creating python environment**
+```powershell
+    C:\path\to\project\root> pip3 install rasa
+    C:\path\to\project\root> python3 -m venv ./venv
+```
+
+### Linux / macOS
+See official documentation [here](https://rasa.com/docs/rasa/installation/).
+
+## Run Book
+Start Bot Steps:
+1. Activate virtual environment from project root:
+    ```powershell
+    .\venv\Scripts\activate
+    ```
+2. (optional after first setup) Train bot:
+    ```powershell
+    rasa train
+    ```
+3. start shell session:
+    ```powershell
+    rasa shell
+    ```
+3. (optional) start interactive shell session:
+    ```powershell
+    rasa interactive
+    ```
+
+## Observations
+- Forced me to do less trial and error and more research
+- 
+
+## Glossary
+
--- actions/actions.py
+++ actions/actions.py
@@ -14,13 +14,37 @@ from rasa_sdk.forms import FormValidationAction
 from rasa_sdk.types import DomainDict
 from rasa_sdk.events import EventType
 
-dumby_data = [
-    {"title": "The Way of Kings", "genre": "fantasy"},
-    {"title": "We are Legion (We are Bob)", "genre": "sci-fi"},
-    {"title": "Stranger in a Strange Land", "genre": "Romance"},
-    {"title": "The Dark Tower", "genre": "Horrer"},
-    {"title": "The DeVincci Code", "genre": "Mystery"},
+ACCEPTED_GENRES = [
+    "fantasy",
+    "sci-fi",
+    "horrer",
+    "mystery",
+    "romance"
 ]
+
+dumby_data = [
+    {"title": "The Way of Kings", "genre": "fantasy", "author": "Brandon Sanderson"},
+    {"title": "We are Legion (We are Bob)",
+     "genre": "sci-fi", "author": "Dennis E Taylor"},
+    {"title": "Stranger in a Strange Land",
+        "genre": "romance", "author": "Robert A. Heinlein"},
+    {"title": "The Dark Tower", "genre": "horrer", "author": "Steven King"},
+    {"title": "The DeVincci Code", "genre": "mystery", "author": "Dan Brown"},
+    {"title": "Project Hail Mary", "genre": "sci-fi", "author": "Andy Weir"},
+    {"title": "The Fifth Season", "genre": "fantasy", "author": "N. K. Jemisin"},
+    {"title": "Columbus Day", "genre": "sci-fi", "author": "Craig Alanson"},
+    {"title": "The Bridge Kingdom", "genre": "romance",
+        "author": "Danielle L. Jensen"},
+    {"title": "The Three-Body Problem", "genre": "horrer", "author": "Cixin Liu"},
+]
+
+
+def fetchData():
+    return dumby_data
+
+
+def clean_input(input):
+    return "".join([c for c in input if c.isalpha()]).lower()
 
 
 class ValidateGenreForm(FormValidationAction):
@@ -36,7 +60,7 @@ class ValidateGenreForm(FormValidationAction):
     ) -> List[EventType]:
         return []
 
-    def validate_name(
+    def validate_genre(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
@@ -45,11 +69,12 @@ class ValidateGenreForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         if not slot_value:
             return {"genre": None}
-
-        # Could also execute another action
-        # using the dispatver, for example:
-        # dispacher.utter_message(text=f"Success! You indicated that you like {genre} books.")
-        return {"genre": slot_value}
+        genre = clean_input(slot_value)
+        if genre not in ACCEPTED_GENRES or len(genre) == 0:
+            dispatcher.utter_message(
+                text=f"Sorry, I didn't understand, \"{slot_value}.\"")
+            return {"genre": None}
+        return {"genre": genre}
 
 
 class ActionSubmitGenreForm(Action):
@@ -64,8 +89,14 @@ class ActionSubmitGenreForm(Action):
         domain: Dict
     ) -> List[EventType]:
         genre = tracker.get_slot("genre")
+        book_data = fetchData()
+        filtered_book_data = filter(
+            lambda book: book["genre"] == genre, book_data)
         dispatcher.utter_message(
             text=f"{genre} is one of my favorite genres of book!")
         dispatcher.utter_message(text=f"Check out these titles ...")
-        dispatcher.utter_message(text=f"TODO")
+        dispatcher.utter_message(text=f"\n")
+        for book in filtered_book_data:
+            dispatcher.utter_message(
+                text=f"\"{book['title']},\" by {book['author']}")
         return []
